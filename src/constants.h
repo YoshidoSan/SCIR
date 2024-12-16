319f39fabb18dd34917c63ea52b2c62ea711eb44
Kacper Marchlewicz <hurin65@gmail.com>
2024-12-16 15:18:29 +0100

    finished

---
+++ src/constants.h
@@ -0,0 +1,8 @@
+const char* ssid = "UPC2A36399";
+const char* password = "BGeukjdy3kvy";
+
+const char* mqttServer = "mqtt3.thingspeak.com";
+const int mqttPort = 1883; // Port MQTT dla ThingSpeak
+const char* clientID = "esp32Client";  // Unikalne ID klienta MQTT
+const char* writeAPIKey = "0M73RU1X0C0P82V0"; // Klucz do zapisu kanału ThingSpeak
+unsigned long channelID = 2718645; // ID kanału ThingSpeak
--- src/main.cpp
+++ src/main.cpp
@@ -1,167 +1,152 @@
+#include "constants.h"
+
 #include <Arduino.h>
-#include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 #include <Adafruit_MCP9808.h>
 
+#include <ThingSpeak.h>
+#include <WiFi.h>
+#include <Wire.h>
 
-// put variable declarations here:
+#include <esp_sleep.h>
+#include <vector>
+
+WiFiClient client;
+
 #define SEALEVELPRESSURE_HPA (1013.25)
-Adafruit_BME280 bme1; //0x76 sdo vdd na 0x77
-Adafruit_BME280 bme2; //0x77
-float bme1_temperature;
-float bme1_humidity;
-float bme1_pressure;
-float bme1_altitude;
-float bme2_temperature;
-float bme2_humidity;
-float bme2_pressure;
-float bme2_altitude;
-Adafruit_MCP9808 mcp1; //0x18, a0 vdd na 0x19
-Adafruit_MCP9808 mcp2; //0x19
-float mcp1_temperature;
-float mcp2_temperature;
+#define DEBUGGING true
 
+float bme1_temperature = 0.0, bme1_humidity = 0.0, bme1_pressure = 0.0, bme1_altitude = 0.0;
+float bme2_temperature = 0.0, bme2_humidity = 0.0, bme2_pressure = 0.0, bme2_altitude = 0.0;
+float mcp1_temperature = 0.0, mcp2_temperature = 0.0;
 
-// put function declarations here:
-void BME1getValues();
-void BME1printValues();
-void MCP1getValues();
-void MCP1printValues();
-void BME2getValues();
-void BME2printValues();
-void MCP2getValues();
-void MCP2printValues();
+Adafruit_BME280 bme1; // 0x76
+Adafruit_BME280 bme2; // 0x77
+Adafruit_MCP9808 mcp1; // 0x18
+Adafruit_MCP9808 mcp2; // 0x19
+
+std::vector<float> BMEgetValues(Adafruit_BME280& bme) {
+  float temperature = bme.readTemperature();
+  float pressure = bme.readPressure() / 100.0F;
+  float altitude = bme.readAltitude(SEALEVELPRESSURE_HPA);
+  float humidity = bme.readHumidity();
+  return {temperature, pressure, humidity, altitude};
+}
+
+float MCPgetValues(Adafruit_MCP9808& mcp) {
+  mcp.wake();
+  float temperature = mcp.readTempC();
+  mcp.shutdown_wake(1);
+  return temperature;
+}
+
+void BMEprintValues(int bme_number, float temperature, float pressure, float humidity, float altitude) {
+  Serial.print("BME number: ");
+  Serial.println(bme_number);
+
+  Serial.print("BME Temperature = ");
+  Serial.print(temperature);
+  Serial.println(" °C");
+
+  Serial.print("BME Pressure = ");
+  Serial.print(pressure);
+  Serial.println(" hPa");
+
+  Serial.print("BME Humidity = ");
+  Serial.print(humidity);
+  Serial.println(" %");
+
+  Serial.print("BME Approx. Altitude = ");
+  Serial.print(altitude);
+  Serial.println(" m");
+  Serial.println();
+}
+
+void MCPprintValues(int mcp_number, float temperature) {
+  Serial.print("MCP number: ");
+  Serial.println(mcp_number);
+
+  Serial.print("MCP Temperature = ");
+  Serial.print(temperature);
+  Serial.println(" °C");
+  Serial.println();
+}
+
+void sent_data() {
+  ThingSpeak.setField(1, bme1_temperature);
+  ThingSpeak.setField(2, bme1_humidity);
+  ThingSpeak.setField(3, bme1_pressure);
+
+  ThingSpeak.setField(4, bme2_temperature);
+  ThingSpeak.setField(5, bme2_humidity);
+  ThingSpeak.setField(6, bme2_pressure);
+
+  ThingSpeak.setField(7, mcp1_temperature);
+  ThingSpeak.setField(8, mcp2_temperature);
+
+  int response = ThingSpeak.writeFields(channelID, writeAPIKey);
+
+  if (DEBUGGING) {
+    if (response == 200) {
+      Serial.println("Dane wysłane do ThingSpeak!");
+    } else {
+      Serial.print("Błąd wysyłania danych, kod błędu: ");
+      Serial.println(response);
+    }
+  }
+}
+
+void collect_sensor_data() {
+  auto bme1_values = BMEgetValues(bme1);
+  bme1_temperature = bme1_values[0];
+  bme1_pressure = bme1_values[1];
+  bme1_humidity = bme1_values[2];
+  bme1_altitude = bme1_values[3];
+
+  auto bme2_values = BMEgetValues(bme2);
+  bme2_temperature = bme2_values[0];
+  bme2_pressure = bme2_values[1];
+  bme2_humidity = bme2_values[2];
+  bme2_altitude = bme2_values[3];
+
+  mcp1_temperature = MCPgetValues(mcp1);
+  mcp2_temperature = MCPgetValues(mcp2);
+
+  sent_data();
+
+  if (DEBUGGING) {
+    BMEprintValues(1, bme1_temperature, bme1_pressure, bme1_humidity, bme1_altitude);
+    BMEprintValues(2, bme2_temperature, bme2_pressure, bme2_humidity, bme2_altitude);
+    MCPprintValues(1, mcp1_temperature);
+    MCPprintValues(2, mcp2_temperature);
+  }
+}
 
 void setup() {
   Serial.begin(115200);
-  // put your setup code here, to run once:
-  Serial.print("Start");
-  // test dioda
-  pinMode(LED_BUILTIN, OUTPUT);
-  // default settings BME
-  if (!bme1.begin(0x76)) {
-    Serial.println("Could not find a valid BME280x76 sensor!");
-  } else {
-    Serial.println("BME280x76 connected");
-    Serial.println();
+  delay(2000);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Łączenie z WiFi...");
   }
-  if (!bme2.begin(0x77)) {
-    Serial.println("Could not find a valid BME280x77 sensor!");
-  } else {
-    Serial.println("BME280x77 connected");
-    Serial.println();
+  Serial.println("Połączono z WiFi!");
+
+  ThingSpeak.begin(client);
+
+  if (!bme1.begin(0x76)) Serial.println("BME280 (0x76) not found!");
+  if (!bme2.begin(0x77)) Serial.println("BME280 (0x77) not found!");
+  if (!mcp1.begin(0x18)) Serial.println("MCP9808 (0x18) not found!");
+  if (!mcp2.begin(0x19)) Serial.println("MCP9808 (0x19) not found!");
+
+  if (esp_sleep_get_wakeup_cause() == ESP_SLEEP_WAKEUP_TIMER) {
+    collect_sensor_data();
   }
-  if (!mcp1.begin(0x18)) {
-    Serial.println("Could not find a valid MCP9808x18 sensor!");
-  } else {
-    Serial.println("MCP9808x18 connected");
-    Serial.println();
-  }
-  if (!mcp2.begin(0x19)) {
-    Serial.println("Could not find a valid MCP9808x19 sensor!");
-  } else {
-    Serial.println("MCP9808x19 connected");
-    Serial.println();
-  }
+
+  esp_sleep_enable_timer_wakeup(30 * 1000000); // 30s
+  esp_deep_sleep_start();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(5000);
-  digitalWrite(LED_BUILTIN, LOW);
-  Serial.println("===========new readings=============");
-  Serial.println("======room 1=======");
-  BME1getValues();
-  BME1printValues();
-  MCP1getValues();
-  MCP1printValues();
-  Serial.println("======room 2=======");
-  BME2getValues();
-  BME2printValues();
-  MCP2getValues();
-  MCP2printValues();
-  Serial.println("====================================");
-  delay(5000);
-}
-
-
-// put function definitions here:
-void BME1getValues() {
-  bme1_temperature = bme1.readTemperature();
-  bme1_pressure = bme1.readPressure() / 100.0F;
-  bme1_altitude = bme1.readAltitude(SEALEVELPRESSURE_HPA);
-  bme1_humidity = bme1.readHumidity();
-}
-
-void BME1printValues() {
-  Serial.print("BME Temperature = ");
-  Serial.print(bme1_temperature);
-  Serial.println(" °C");
-
-  Serial.print("BME Pressure = ");
-  Serial.print(bme1_pressure);
-  Serial.println(" hPa");
-
-  Serial.print("BME Approx. Altitude = ");
-  Serial.print(bme1_altitude);
-  Serial.println(" m");
-
-  Serial.print("BME Humidity = ");
-  Serial.print(bme1_humidity);
-  Serial.println(" %");
-  Serial.println();
-}
-
-void BME2getValues() {
-  bme2_temperature = bme2.readTemperature();
-  bme2_pressure = bme2.readPressure() / 100.0F;
-  bme2_altitude = bme2.readAltitude(SEALEVELPRESSURE_HPA);
-  bme2_humidity = bme2.readHumidity();
-}
-
-void BME2printValues() {
-  Serial.print("BME Temperature = ");
-  Serial.print(bme2_temperature);
-  Serial.println(" °C");
-
-  Serial.print("BME Pressure = ");
-  Serial.print(bme2_pressure);
-  Serial.println(" hPa");
-
-  Serial.print("BME Approx. Altitude = ");
-  Serial.print(bme2_altitude);
-  Serial.println(" m");
-
-  Serial.print("BME Humidity = ");
-  Serial.print(bme2_humidity);
-  Serial.println(" %");
-  Serial.println();
-}
-
-void MCP1getValues() {
-  mcp1.wake();
-  mcp1_temperature = mcp1.readTempC();
-  mcp1.shutdown_wake(1);
-}
-void MCP1printValues() {
-  Serial.println();
-  Serial.print("MCP Temperature = ");
-  Serial.print(mcp1_temperature);
-  Serial.println(" °C");
-  Serial.println();
-}
-
-void MCP2getValues() {
-  mcp2.wake();
-  mcp2_temperature = mcp2.readTempC();
-  mcp2.shutdown_wake(1);
-}
-void MCP2printValues() {
-  Serial.println();
-  Serial.print("MCP Temperature = ");
-  Serial.print(mcp2_temperature);
-  Serial.println(" °C");
-  Serial.println();
-}
+void loop() {}
